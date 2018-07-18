b377f2e06779aaa1e90b655a7ae67e657448dfca
ppcrong <ppcrong@gmail.com>
2018-07-18 18:05:37 +0800

    [ppcrong.20180718][BLETOOLBOX] Add BleBatteryManager

---
+++ app/src/main/java/com/ppcrong/bletoolbox/battery/BleBatteryManager.java
@@ -0,0 +1,8 @@
+package com.ppcrong.bletoolbox.battery;
+
+import java.util.UUID;
+
+public class BleBatteryManager {
+    public final static UUID BATTERY_SERVICE = UUID.fromString("0000180F-0000-1000-8000-00805f9b34fb");
+    public final static UUID BATTERY_LEVEL_CHARACTERISTIC = UUID.fromString("00002A19-0000-1000-8000-00805f9b34fb");
+}
