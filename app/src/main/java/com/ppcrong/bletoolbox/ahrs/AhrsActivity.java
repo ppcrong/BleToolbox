508158e4ef816fb6eac2f7b93c2a28e296def54d
ppcrong <ppcrong@gmail.com>
2018-09-10 16:03:08 +0800

    [ppcrong.20180910][BLETOOLBOX] Sync with Frank for correct euler calculation

--- app/src/main/java/com/ppcrong/bletoolbox/ahrs/AhrsActivity.java
+++ app/src/main/java/com/ppcrong/bletoolbox/ahrs/AhrsActivity.java
@@ -110,9 +110,9 @@ public class AhrsActivity extends ProfileBaseActivity {
 
         // region [Euler]
         offset += 2;
-        final int ez = -ValueInterpreter.getIntValue(bytes, ValueInterpreter.FORMAT_SINT16, offset); // ALG x
+        final int ez = ValueInterpreter.getIntValue(bytes, ValueInterpreter.FORMAT_SINT16, offset); // ALG x
         offset += 2;
-        final int ex = ValueInterpreter.getIntValue(bytes, ValueInterpreter.FORMAT_SINT16, offset); // ALG y
+        final int ex = -ValueInterpreter.getIntValue(bytes, ValueInterpreter.FORMAT_SINT16, offset); // ALG y
         offset += 2;
         final int ey = -ValueInterpreter.getIntValue(bytes, ValueInterpreter.FORMAT_SINT16, offset); // ALG z
         // endregion [Euler]
